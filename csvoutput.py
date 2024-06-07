c218b45db381f602242709c88b8da46489f9f8e6
andretdr <neckoid@hotmail.com>
2024-06-07 22:05:26 +0800

    cleaned up and updated search list

--- csvoutput.py
+++ csvoutput.py
@@ -2,9 +2,9 @@ from csv import reader
 
 f = open("./stock_info.js", 'a')
 
-with open("./static/stock_info.csv", 'r') as data:
+with open("./static/stock_info_070624.csv", 'r') as data:
     for line in reader(data):
-        f.write(str(line))
+        f.write(str('["'+line[0]+'", "'+line[1]+'"]'))
         f.write(", \n")
 f.close()
 
